08d708e83b61aca9cde7d2eda7a50b92d4c399ae
Sabrina Jiang <sabrina.jiang813@gmail.com>
2019-01-06 22:26:55 -0800

    Learned Http chapter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
@@ -8,6 +11,7 @@ import { EventDetailComponent } from './event-detail/event-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { EventSearchComponent } from './event-search/event-search.component';
 
 // Every component must be declared in exactly one NgModule.
 @NgModule({
@@ -16,12 +20,20 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     EventsComponent,
     EventDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    EventSearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+// and returns simulated server responses.
+// Remove it when a real server is ready to receive requests.
+  HttpClientInMemoryWebApiModule.forRoot(
+    InMemoryDataService, { dataEncapsulation: false }
+  )
   ],
   providers: [],
   bootstrap: [AppComponent]
